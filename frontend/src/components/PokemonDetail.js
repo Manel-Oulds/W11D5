22ecd3432574755d9f1dc95060b0a990b451ee13
Manel-Os <oulds.manel@gmail.com>
2023-07-14 16:41:37 -0400

    Phase 3

--- frontend/src/components/PokemonDetail.js
+++ frontend/src/components/PokemonDetail.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PokemonItems from './PokemonItems';
 import EditPokemonForm from './EditPokemonForm';
 import ItemForm from './ItemForm';
+import { addingPokemon, getOnePokemon } from '../store/pokemon';
 
 const PokemonDetail = () => {
+  const dispatch = useDispatch()
   const { pokemonId } = useParams();
   const pokemon = useSelector(state => state.pokemon[pokemonId]);
   const [showEditPokeForm, setShowEditPokeForm] = useState(false);
@@ -14,6 +16,8 @@ const PokemonDetail = () => {
   useEffect(() => {
     setShowEditPokeForm(false);
     setEditItemId(null);
+    dispatch(getOnePokemon(pokemonId))
+    
   }, [pokemonId]);
 
   if (!pokemon || !pokemon.moves) {
