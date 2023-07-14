e2bbf3d072e7fd573b91aac0354e769e04d2e63e
Manel-Os <oulds.manel@gmail.com>
2023-07-14 14:13:23 -0400

    Phase 0

---
+++ frontend/src/components/PokemonItems.js
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemon } from "../store/pokemon";
+import { useEffect } from "react";
+
+const PokemonItems = ({ pokemon, setEditItemId }) => {
+ 
+ 
+  const items = useSelector((state) => {
+    if (!pokemon.items) return null;
+    return pokemon.items.map(itemId => state.items[itemId]);
+  });
+
+  if (!items) {
+    return null;
+  }
+
+  return items.map((item) => (
+    <tr key={item.id}>
+      <td>
+        <img
+          className="item-image"
+          alt={item.imageUrl}
+          src={`${item.imageUrl}`}
+        />
+      </td>
+      <td>{item.name}</td>
+      <td className="centered">{item.happiness}</td>
+      <td className="centered">${item.price}</td>
+      {pokemon.captured && (
+        <td className="centered">
+          <button onClick={() => setEditItemId(item.id)}>
+            Edit
+          </button>
+        </td>
+      )}
+      {pokemon.captured && (
+        <td className="centered">
+          <button>
+            Delete
+          </button>
+        </td>
+        
+      )}
+    </tr>
+  ));
+};
+
+export default PokemonItems;
